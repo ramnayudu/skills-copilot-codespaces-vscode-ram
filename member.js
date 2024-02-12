00bf0b55ccc2c80da0d360cd53a97a4ad832917e
Ram Nayudu <nsriram.sdk@gmail.com>
2024-02-12 06:09:47 +0000

    third push

--- member.js
+++ member.js
@@ -1,3 +1,4 @@
-function skillsmember() {
-  console.log('skillsmember');
+function skillsmember(){
+    var member = document.getElementById('member');
+    member.innerHTML = 'Skills: <br> 1. HTML <br> 2. CSS <br> 3. JavaScript <br> 4. React <br> 5. Node.js <br> 6. MongoDB';
 }
